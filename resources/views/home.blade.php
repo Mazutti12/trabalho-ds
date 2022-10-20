06eb49b52c6dec31295ee53ada4978339f9148df
Victor Mazutti <victor.150305@bento.local>
2022-10-20 16:27:34 -0300

    Arrumando comonents

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,64 +1,7 @@
-@extends('components.header')
+@extends('principal.header')
 
 @section('container')
-    <nav class="navbar bg-light fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Nome Empresa</a>
-            <a class="navbar-brand" href="#">Home</a>
-            <a class="navbar-brand" href="#">About</a>
-            <a class="navbar-brand" href="#">Outros</a>
-            <a class="navbar-brand btn-success" href="#">Agendar </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    @include('components.navBar')
 
-            @auth
 
-                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Icone de Perfil - nome usuário</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Perfil</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Agendas</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Configurações</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Log Out</a>
-                    </div>
-                    </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-    @endauth
-
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Ainda não está cadastrado?</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('login') }}">Entre</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('cadastro') }}">Cadastre-se</a>
-                </li>
-        </div>
-        </li>
-        </ul>
-    </div>
-    </div>
-    </nav>
 @endsection
