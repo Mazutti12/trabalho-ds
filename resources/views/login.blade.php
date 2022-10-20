6b3b85470798c87877d074425c2f32a3c98a25e7
Bernardo Marquetti <bernardo.060404@bento.local>
2022-10-20 16:54:03 -0300

    login e cadastro finalizado

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,42 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-
-<body>
-    @if (session('erro'))
-    <div>
-        {{ session('erro') }}
+@include('principal.header')
+@include('components.navBar')
+@if (session('erro'))
+<div>
+    {{ session('erro') }}
+</div>
+@endif
+<form action="{{ route('login') }}" method="POST">
+    @csrf
+    <div class="mb-3 mt-3">
+        <label for="login">Login:</label>
+        <input type="text" name="login" id="login" class="form-control" placeholder="Entre com seu login" required>
+        <!-- <div class="valid-feedback">Válido.</div>
+            <div class="invalid-feedback">Inválido.</div> -->
     </div>
-    @endif
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        <div class="mb-3 mt-3">
-            <label for="login">Login:</label>
-            <input type="text" name="login" id="login" class="form-control" placeholder="Entre com seu login" required>
-            <!-- <div class="valid-feedback">Válido.</div>
+    <div class="mb-3">
+        <label for="password">Senha:</label>
+        <input type="password" class="form-control" name="password" id="password" placeholder="Entre com sua senha" required>
+        <!-- <div class="valid-feedback">Válido.</div>
             <div class="invalid-feedback">Inválido.</div> -->
-        </div>
-        <div class="mb-3">
-            <label for="password">Senha:</label>
-            <input type="password" class="form-control" name="password" id="password" placeholder="Entre com sua senha" required>
-            <!-- <div class="valid-feedback">Válido.</div>
-            <div class="invalid-feedback">Inválido.</div> -->
-        </div>
-        <div class="mb-3">
-            <a href="{{ route('index') }}" class="text-danger" target="_blank">Não possuo cadastro</a>
-        </div>
+    </div>
+    <div class="mb-3">
+        <a href="{{ route('index') }}" class="text-danger" target="_blank">Não possuo cadastro</a>
+    </div>
 
-        <div class="mb-3">
-            <input name="submit" id="botao" type="submit" value="Login" />
+    <div class="mb-3 col-8">
+            <button type="submit" class="btn btn-danger" name="submit" value="Register">Login</button>
         </div>
-
-    </form>
-</body>
-
-</html>
+</form>
