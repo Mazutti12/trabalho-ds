c5bbd4968e4b0fee5c23a944f16995e3777825c9
Bernardo Marquetti <bernardo.060404@bento.local>
2022-11-08 15:36:57 -0300

    send email to log

---
+++ public/teste.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang=3D"en">
+
+<head>
+    <meta charset=3D"UTF-8">
+    <meta http-equiv=3D"X-UA-Compatible" content=3D"IE=3Dedge">
+    <meta name=3D"viewport" content=3D"width=3Ddevice-width, initial-scale=
+=3D1.0">
+    <title>teste</title>
+</head>
+
+<body>
+    <h3>Email</h3>
+    <p>Hello friends, Welcome to Cambo Tutorial Mail Delivery!</p>
+</body>
+
+</html>  
